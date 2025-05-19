ef3633d11009b08924abdb989608a6a887f92d2f
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-05-18 21:06:02 -0400

    add tests for table, function, and union

--- tests/test_unit.lua
+++ tests/test_unit.lua
@@ -2,6 +2,7 @@ local eq = MiniTest.expect.equality
 local validator = require "lua.schema-validator.init"
 local validate = validator.validate
 local str_literal = validator.str_literal
+local union = validator.union
 
 local T = MiniTest.new_set()
 
@@ -83,6 +84,167 @@ end
 T["str_literal"]["should handle optional"] = function()
   eq(validate({ type = str_literal "hello", }, nil), false)
   eq(validate({ type = str_literal "hello", optional = true, }, nil), true)
+end
+
+T["table"] = MiniTest.new_set()
+T["table"]["array"] = MiniTest.new_set()
+T["table"]["array"]["should return true for tables where every value matches the entries type"] = function()
+  eq(validate({ type = "table", entries = "number", }, {}), true)
+  eq(validate({ type = "table", entries = "number", }, { 1, 2, 3, }), true)
+  eq(validate({ type = "table", entries = "number", }, { 1, nil, 3, }), true)
+end
+T["table"]["array"]["should return false for tables where not every value matches the entries type"] = function()
+  eq(validate({ type = "table", entries = "number", }, { 1, 2, "hello", }), false)
+end
+T["table"]["array"]["should return false for non-tables"] = function()
+  eq(validate({ type = "table", entries = "number", }, "there"), false)
+  eq(validate({ type = "table", entries = "number", }, function() end), false)
+  eq(validate({ type = "table", entries = "number", }, 123), false)
+  eq(validate({ type = "table", entries = "number", }, true), false)
+end
+T["table"]["array"]["should handle optional"] = function()
+  eq(validate({ type = "table", entries = "number", }, nil), false)
+  eq(validate({ type = "table", entries = "number", optional = true, }, nil), true)
+end
+
+T["table"]["tuple"] = MiniTest.new_set()
+T["table"]["tuple"]["should return true for tables where every value matches the entries"] = function()
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, { "hello", 1, }), true)
+end
+T["table"]["tuple"]["should return false for tables where not every value matches the entries"] = function()
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, {}), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, { "hello", }), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, { "hello", 1, 2, }), false)
+end
+T["table"]["tuple"]["should return false for non-tables"] = function()
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, "there"), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, function() end), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, 123), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, true), false)
+end
+T["table"]["tuple"]["should handle optional"] = function()
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+  }, nil), false)
+  eq(validate({
+    type = "table",
+    entries = {
+      { type = "string", },
+      { type = "number", },
+    },
+    optional = true,
+  }, nil), true)
+end
+
+T["functions"] = MiniTest.new_set()
+T["functions"]["should return true when the fn returns true"] = function()
+  eq(validate({
+    type = function()
+      return true
+    end,
+  }, "hello"), true)
+  eq(validate({
+    type = function(val)
+      return validate({ type = "string", }, val)
+    end,
+  }, "hello"), true)
+end
+T["functions"]["should return false when the fn returns false"] = function()
+  eq(validate({
+    type = function()
+      return false
+    end,
+  }, 123), false)
+  eq(validate({
+    type = function(val)
+      return validate({ type = "string", }, val)
+    end,
+  }, 123), false)
+end
+
+T["union"] = MiniTest.new_set()
+T["union"]["should return true when one of the schemas is true"] = function()
+  eq(
+    validate({
+      type = union {
+        { type = "string", },
+        { type = "boolean", },
+      },
+    }, "hello"),
+    true
+  )
+  eq(
+    validate({
+      type = union {
+        { type = "string", },
+        { type = "boolean", },
+      },
+    }, false),
+    true
+  )
+end
+T["union"]["should return false when both schemas are false"] = function()
+  eq(
+    validate({
+      type = union {
+        { type = "string", },
+        { type = "boolean", },
+      },
+    }, 123),
+    false
+  )
 end
 
 return T
